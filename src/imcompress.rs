d4d5c03c08e76efb288f0acf0cb851dcbff0ecf3
cruzzil <1434309+cruzzil@users.noreply.github.com>
2026-06-05 22:42:26 +0800

    Sync 28ede81db7d18b300e3171d88e2de47a0b7bdfce

--- src/imcompress.rs
+++ src/imcompress.rs
@@ -44,7 +44,9 @@ use crate::getcold::{
 use crate::getcole::{fffi1r4, fffi2r4, fffi4r4, fffr4r4, fffr4r4_inplace, fffr8r4, ffgsve_safe};
 use crate::getcoli::{fffi1i2, fffi2i2, fffi4i2, fffi8i2, fffr4i2, fffr8i2, ffgsvi_safe};
 use crate::getcolj::{fffi1i4, fffi2i4, fffi4i4, fffr4i4, fffr8i4};
-use crate::getcolk::{fffi1int, fffi2int, fffi4int, fffi8int, fffr4int, fffr8int, ffgcvk_safe, ffgsvk_safe};
+use crate::getcolk::{
+    fffi1int, fffi2int, fffi4int, fffi8int, fffr4int, fffr8int, ffgcvk_safe, ffgsvk_safe,
+};
 use crate::getcolsb::{fffi1s1, fffi2s1, fffi4s1};
 use crate::getcolui::{fffi1u2, fffi2u2, fffi4u2, fffr4u2, fffr8u2};
 use crate::getcoluj::{fffi1u4, fffi2u4, fffi4u4, fffr4u4, fffr8u4};
@@ -8532,7 +8534,6 @@ fn imcomp_decompress_tile(
         pixlen = mem::size_of::<c_short>();
 
         if (infptr.Fptr).quantize_level == NO_QUANTIZE {
-            /* the floating point pixels were losselessly compressed with GZIP */
             /* Just have to copy the values to the output array */
 
             if tiledatatype == TINT {
@@ -8543,6 +8544,34 @@ fn imcomp_decompress_tile(
                     bzero,
                     nullcheck,
                     tnull,
+                    nulval.get_value_as_f64() as c_short,
+                    bnullarray,
+                    anynul.as_deref_mut(),
+                    cast_slice_mut(buffer),
+                    status,
+                );
+            } else if (tiledatatype == TSHORT) {
+                fffi2i2(
+                    cast_slice_mut(idata),
+                    c_long::from(tilelen),
+                    bscale,
+                    bzero,
+                    nullcheck,
+                    tnull as c_short,
+                    nulval.get_value_as_f64() as c_short,
+                    bnullarray,
+                    anynul.as_deref_mut(),
+                    cast_slice_mut(buffer),
+                    status,
+                );
+            } else if (tiledatatype == TBYTE) {
+                fffi1i2(
+                    cast_slice_mut(idata),
+                    c_long::from(tilelen),
+                    bscale,
+                    bzero,
+                    nullcheck,
+                    tnull as c_uchar,
                     nulval.get_value_as_f64() as c_short,
                     bnullarray,
                     anynul.as_deref_mut(),
